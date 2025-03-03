a9231053654cd9b95bc23407c5802bf1b50d0e2e
Mathew Petro <petromathew@gmail.com>
2025-03-02 19:29:14 -0500

    Removed failing code

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,12 +1,10 @@
 "use client"; // Required if using Next.js app router with interactivity
 
 import React from 'react';
-import PhotoLabelingTool from '../../components/PhotoLabelingTool';
 
 const Home: React.FC = () => {
     return (
       <>
-        <PhotoLabelingTool imageUrl='https://www.hok.com/wp-content/uploads/2019/05/Suvarnabhumi-International-Airport-Midfield-Satellite-Concourse_Aerial_1900-1600x1069.jpg' onSubmit={()=>{}} />
       </>
     );
 };
