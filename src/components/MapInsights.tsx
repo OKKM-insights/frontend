41b802f76381c62a824208538ecb96362d26048d
OlegGlo <olegglotov99@gmail.com>
2025-03-28 18:08:47 -0400

    eslint fight

--- src/components/MapInsights.tsx
+++ src/components/MapInsights.tsx
@@ -1,34 +1,12 @@
 import React, { useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
-import { PieChart, Pie, Cell } from 'recharts';
-import { ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
-import { DataInsightsProps } from '@/types';
 
 interface MapInsightsProps {
     id: string | string[];
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const PercentTooltipContent = ({ active, payload }: any) => {
-    console.log(payload)
-    if (active && payload && payload.length) {
-        return (
-            <div className="rounded-lg border bg-background p-2 shadow-sm">
-                <div className="flex flex-col gap-0.5 text-center">
-                    <span className="text-muted-foreground">{payload[0].name}</span>
-                    <span className="font-bold text-black">{`${payload[0].value}%`}</span>
-                </div>
-            </div>
-        )
-    }
-    return null
-}
-
 export default function MapInsights({ id }: MapInsightsProps) {
-    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
     const [imageError, setImageError] = useState(false);
-    const project_id = id;
     return (
         <Card className="col-span-2 bg-gray-800 text-white h-fit">
             <CardHeader>
